5642b246f88c372781ae3ae630831dc2fb8beda8
Serhii Berdianskyi <zeferot94@gmail.com>
2025-05-14 12:16:40 +0300

    created EventViewSet

--- event_management/views.py
+++ event_management/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from rest_framework import viewsets
 
-# Create your views here.
+from event_management.models import Event
+from event_management.serializers import EventSerializer
+
+
+class EventViewSet(viewsets.ModelViewSet):
+    """
+    A viewset for viewing and editing event instances.
+    """
+
+    queryset = Event.objects.all()
+    serializer_class = EventSerializer
