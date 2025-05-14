03a3fe5329ae0fdfb12cffe71ae274d4fd019cd2
Serhii Berdianskyi <zeferot94@gmail.com>
2025-05-14 12:16:22 +0300

    added event_management/urls.py to  event_management_api/urls.py

--- event_management_api/urls.py
+++ event_management_api/urls.py
@@ -16,8 +16,12 @@ Including another URLconf
 """
 
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path(
+        "api/event_management/",
+        include("event_management.urls", namespace="event_management"),
+    ),
 ]
