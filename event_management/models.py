b2aa4707a88011e54300e4128f4cc56557c6e931
Serhii Berdianskyi <zeferot94@gmail.com>
2025-05-14 11:29:35 +0300

    added Event model

--- event_management/models.py
+++ event_management/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+
+class Event(models.Model):
+    title = models.CharField(max_length=255)
+    description = models.TextField()
+    date = models.DateTimeField()
+    location = models.CharField(max_length=255)
+    organizer = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.title
