6706e3f3220f8945e95cd3dec344253db90cf056
Serhii Berdianskyi <zeferot94@gmail.com>
2025-05-14 21:55:23 +0300

    added model for Event Registration

--- event_management/models.py
+++ event_management/models.py
@@ -10,3 +10,20 @@ class Event(models.Model):
 
     def __str__(self):
         return self.title
+
+
+class EventRegistration(models.Model):
+    event = models.ForeignKey(
+        Event, on_delete=models.CASCADE, related_name="registrations"
+    )
+    user = models.ForeignKey(
+        "user.User", on_delete=models.CASCADE, related_name="event_registrations"
+    )
+    registration_date = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        constraints = [
+            models.UniqueConstraint(
+                fields=["event", "user"], name="unique_event_registration"
+            )
+        ]
