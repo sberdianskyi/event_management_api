6fa7485cc18658639ed598d6513d733b36c633ca
Serhii Berdianskyi <zeferot94@gmail.com>
2025-05-14 21:59:07 +0300

    registered endpoint for registration

--- event_management/urls.py
+++ event_management/urls.py
@@ -1,12 +1,13 @@
 from django.urls import path, include
 from rest_framework import routers
 
-from event_management.views import EventViewSet
+from event_management.views import EventViewSet, EventRegistrationViewSet
 
 
 app_name = "event_management"
 
 router = routers.DefaultRouter()
 router.register("events", EventViewSet)
+router.register("registrations", EventRegistrationViewSet)
 
 urlpatterns = [path("", include(router.urls))]
