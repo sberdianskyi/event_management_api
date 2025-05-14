c7c3764559831236fd3e48e10d41bb5a478a4101
Serhii Berdianskyi <zeferot94@gmail.com>
2025-05-14 15:10:07 +0300

    created CreateUserView and LoginUserView

--- user/views.py
+++ user/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from rest_framework import generics
+from rest_framework.authtoken.views import ObtainAuthToken
+from rest_framework.permissions import AllowAny
+from rest_framework.settings import api_settings
 
-# Create your views here.
+
+from user.serializers import UserSerializer
+
+
+class CreateUserView(generics.CreateAPIView):
+    serializer_class = UserSerializer
+    permission_classes = (AllowAny,)
+
+
+class LoginUserView(ObtainAuthToken):
+    renderer_classes = api_settings.DEFAULT_RENDERER_CLASSES
